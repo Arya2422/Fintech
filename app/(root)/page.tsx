c3a612ea9f78f20bf8378fdea5cb70491bd2a93e
Arya2422 <saliarya24@gmail.com>
2024-11-08 03:04:59 +0530

    implemented some changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import RightSidebar from '@/components/RightSidebar';
 
-const Home = () => {
+const Home = ({user}) => {
+  const loggedIn={firstName:'ABC',email:'abc@gmail.com'};
+  
+  
+
   return (
-    <div>
-   Home
-    </div>
+    <section className='home'>
+     <div className='home-content'>
+      <header className='home-header'>
+      <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+  <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1455}
+          />
+      </header>
+        RECENT TRANSACTIONS, <b>TODAY</b>
+     </div>
+     <RightSidebar 
+      user={loggedIn}
+      transactions={[]} 
+      banks={[{currentBalance:1455.00}, 
+              {currentBalance:1455.00}]
+     }/>
+     </section>
   )
 }
 
